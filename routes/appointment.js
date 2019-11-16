d7f86229f940640fde552e86f555fedd69cd2c6e
Mohit Jha <mohitkumarjha56@gmail.com>
2019-11-16 22:51:54 +0530

    added loader, view appointment

--- routes/appointment.js
+++ routes/appointment.js
@@ -3,46 +3,47 @@ var router = express.Router();
 var firebase = require('firebase');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   //console.log(req.query);
 
-  firebase.database().ref('/appointments/doctor/'+req.query.uid).once('value',function(snap){
+  firebase.database().ref('/appointments/doctor/' + req.query.uid).once('value', function (snap) {
     var result = snap.val();
 
-    var APP = [];
+    var APP1 = [];
+    var APP2 = [];
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    var today = dd + '-' + mm + '-' + yyyy;
 
     for (key in result) {
       var res2 = result[key];
-      for (key2 in res2){
-        APP.push(res2[key2]);
+      for (key2 in res2) {
+        if(res2[key2].date == today)
+          APP1.push(res2[key2]);
+        else
+          APP2.push(res2[key2]);
       }
     }
 
-    res.render('appointment',{title:"Appointments",appointments:APP});
+    res.render('appointment', { title: "Appointments", appointments: APP1, app2 : APP2 });
 
   });
 
-  
-});
-
-router.post('/accept',function(req,res){
-  
-  var pfuid = req.body.appointment.pfuid;
-  var dfuid = req.body.appointment.dfuid;
-  var apid = req.body.appointment.apid;
-  var date = req.body.appointment.date;
-
-  var updates = {};
-  updates['/appointments/patients/'+pfuid+'/'+apid+'/status'] = "accepted";
-  updates['/appointments/doctor/'+dfuid+'/'+date+'/'+apid+'/status'] = "accepted";
-
-  firebase.database().ref().update(updates);
-  res.send("ok");
 
 });
 
-router.post('/reject',function(req,res){
+router.post('/accept', function (req, res) {
 
   var pfuid = req.body.appointment.pfuid;
   var dfuid = req.body.appointment.dfuid;
@@ -50,12 +51,36 @@ router.post('/reject',function(req,res){
   var date = req.body.appointment.date;
 
   var updates = {};
-  updates['/appointments/patients/'+pfuid+'/'+apid+'/status'] = "rejected";
-  updates['/appointments/doctor/'+dfuid+'/'+date+'/'+apid+'/status'] = "rejected";
+  updates['/appointments/patients/' + pfuid + '/' + apid + '/status'] = "accepted";
+  updates['/appointments/doctor/' + dfuid + '/' + date + '/' + apid + '/status'] = "accepted";
 
   firebase.database().ref().update(updates);
   res.send("ok");
 
+});
+
+router.post('/reject', function (req, res) {
+
+  var pfuid = req.body.appointment.pfuid;
+  var dfuid = req.body.appointment.dfuid;
+  var apid = req.body.appointment.apid;
+  var date = req.body.appointment.date;
+
+  var updates = {};
+  updates['/appointments/patients/' + pfuid + '/' + apid + '/status'] = "rejected";
+  updates['/appointments/doctor/' + dfuid + '/' + date + '/' + apid + '/status'] = "rejected";
+
+  firebase.database().ref().update(updates);
+  res.send("ok");
+
+});
+
+router.post('/reschedule', function (req, res) {
+  var appointment = req.body.appointment;
+  console.log(appointment);
+
+  firebase.database().ref('/app')
+  res.send("ok");
 });
 
 module.exports = router;
